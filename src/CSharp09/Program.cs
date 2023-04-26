73d332d61ef4903096dc17f39426c851cfd98ea8
IYEMON018 <iyemon018@gmail.com>
2023-04-27 00:43:22 +0900

    wip デバッグ実行できるところまで実装した。

--- src/CSharp09/Program.cs
+++ src/CSharp09/Program.cs
@@ -2,7 +2,9 @@
 
 // 以下の手順を試す。
 // cf. https://learn.microsoft.com/ja-jp/dotnet/csharp/roslyn-sdk/source-generators-overview#get-started-with-source-generators
-
+// こっちも参考になりそう。
+// 以下には SourceGenerator のデバッグ手順が記載されているので、こちらは必ず設定すること。
+// cf. https://neue.cc/2022/12/16_IncrementalSourceGenerator.html
 namespace CSharp09
 {
     public static partial class Program
---
+++ src/SourceGenerator/SampleGenerator.cs
@@ -0,0 +1,34 @@
+﻿namespace SourceGenerator
+{
+    using Microsoft.CodeAnalysis;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// 以下をベースに作成している。
+    /// cf. https://neue.cc/2022/12/16_IncrementalSourceGenerator.html
+    /// </remarks>
+    [Generator(LanguageNames.CSharp)]
+    public class SampleGenerator : IIncrementalGenerator
+    {
+        public SampleGenerator()
+        {
+            System.Diagnostics.Debugger.Launch();
+        }
+
+        /// <summary>
+        /// Called to initialize the generator and register generation steps via callbacks
+        /// on the <paramref name="context" />
+        /// </summary>
+        /// <param name="context">The <see cref="T:Microsoft.CodeAnalysis.IncrementalGeneratorInitializationContext" /> to register callbacks on</param>
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            //
+            context.RegisterPostInitializationOutput(static context =>
+            {
+                context.AddSource("SampleGenerated.g.cs", "// test");
+            });
+        }
+    }
+}
