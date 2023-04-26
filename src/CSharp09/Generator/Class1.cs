d7c64da665ac652746a0a418c133ed76892301ba
IYEMON018 <iyemon018@gmail.com>
2023-04-26 22:17:02 +0900

    wip add 新規プロジェクト

---
+++ src/CSharp09/Generator/Class1.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Generator
+{
+    public class Class1
+    {
+
+    }
+}
--- src/CSharp09/Program.cs
+++ src/CSharp09/Program.cs
@@ -1,5 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// 以下の手順を試す。
+// cf. https://learn.microsoft.com/ja-jp/dotnet/csharp/roslyn-sdk/source-generators-overview#get-started-with-source-generators
+
 using System;
 
-Console.WriteLine("Hello, World!");
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    static partial void HelloFrom(string name);
+}
